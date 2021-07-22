d8886d31960f6fe7f3656e99f37426a5c6ca763c
wilson614 <s110613053@stu.ntue.edu.tw>
2021-07-23 01:24:13 +0800

    Modify index.handlebars with css

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const port = 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 app.use(methodOverride('_method'))
 app.use(routes)
 
