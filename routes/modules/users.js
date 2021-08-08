8d371b70c1ac84e36c7e32e4859d6dad159ad749
wilson614 <s110613053@stu.ntue.edu.tw>
2021-08-08 20:37:24 +0800

    Add user register page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/login', (req, res) => {
-  res.render('Login')
+  res.render('login')
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
